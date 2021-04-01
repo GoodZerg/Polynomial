5973e6c37cffe42a3f165459c5502cbdc377a432
[ sora. ] <57730505+GoodZerg@users.noreply.github.com>
2021-04-01 11:52:18 +0300

    hiihihihihi

--- Polynom.h
+++ Polynom.h
@@ -8,15 +8,15 @@ public:
 	Polynom(std::vector<Monom*>&);
 
 	template<typename ...Args>
-	Polynom(Args...);
+	Polynom(Args&&...);
 
 	~Polynom();
 
 	template<typename T>
-	void pushToNominals(T first) noexcept;
+	void pushToNominals(T&& first) noexcept;
 
 	template<typename T, typename ...Args>
-	void pushToNominals(T first, Args... args) noexcept;
+	void pushToNominals(T&& first, Args&&... args) noexcept;
 
 private:
 
@@ -28,7 +28,7 @@ inline Polynom::Polynom(std::vector<Monom*>& arr) {
 }
 
 template<typename ...Args>
-inline Polynom::Polynom(Args... args) {
+inline Polynom::Polynom(Args&&... args) {
 	this->pushToNominals(args...);
 }
 
@@ -37,12 +37,12 @@ Polynom::~Polynom()
 }
 
 template<typename T>
-inline void Polynom::pushToNominals(T first) noexcept {
-	this->nominals.push_back(first);
+inline void Polynom::pushToNominals(T&& first) noexcept {
+	this->nominals.push_back(&first);
 }
 
 template<typename T, typename ...Args>
-inline void Polynom::pushToNominals(T first, Args ...args) noexcept {
-	this->nominals.push_back(first);
+inline void Polynom::pushToNominals(T&& first, Args&&... args) noexcept {
+	this->nominals.push_back(&first);
 	this->pushToNominals(args...);
 }
